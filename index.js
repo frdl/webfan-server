843cf71b488bc82a8be29613f5f8d0932e43e5e0
Till Wehowski <github@webfan.de>
2021-06-14 21:43:18 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const { spawnSync } = require('child_process');
+
+////spawnSync(process.execPath, process.argv.slice(2), { stdio: 'inherit' })
+
+
+//process.chdir(__dirname);
+//require( "./server");
+
+spawnSync(process.execPath,  ['frdl', 'autoload', './server'], { stdio: 'inherit' });
