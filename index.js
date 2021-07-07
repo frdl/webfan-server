2c55bf0aae0f8b672e81b9b16f4b056901877ecc
Till Wehowski <github@webfan.de>
2021-07-07 20:00:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 
 module.exports.create = require( "./server").create;
-module.exports.logger = =require('./logging');
-module.exports.redwire = =require('redwire');
-module.exports.ip = =require('ip');
-module.exports.redbird = =require('redbird');
-module.exports.finalhandler = =require('finalhandler');
+module.exports.logger = require('./logging');
+module.exports.redwire = require('redwire');
+module.exports.ip = require('ip');
+module.exports.redbird = require('redbird');
+module.exports.finalhandler = require('finalhandler');
 
 
