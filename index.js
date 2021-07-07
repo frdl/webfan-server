c02cba59908436429a82c3f4196fd28bf22e8471
Till Wehowski <github@webfan.de>
2021-07-07 19:38:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-/*
-const { spawnSync } = require('child_process');
 
-////spawnSync(process.execPath, process.argv.slice(2), { stdio: 'inherit' })
-
-
-//process.chdir(__dirname);
-//require( "./server");
-
-spawnSync(process.execPath,  ['frdl', 'autoload', './server.js'], { stdio: 'inherit' });
-*/
 
 module.exports.create = require( "./server").create;
+module.exports.logger = =require('./logging');
+module.exports.redwire = =require('redwire');
+module.exports.ip = =require('ip');
+module.exports.redbird = =require('redbird');
+module.exports.finalhandler = =require('finalhandler');
+
+
