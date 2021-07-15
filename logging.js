3559794a4fcc971b71c94d123421665b28107085
Till Wehowski <github@webfan.de>
2021-07-15 18:20:38 +0200

    Update logging.js

--- logging.js
+++ logging.js
@@ -1,7 +1,7 @@
 
 
 var config = require('./get-configuration');
-
+var fs = require('fs');
 var winston = require('winston');
   require('winston-syslog').Syslog;
   require('winston-daily-rotate-file');
@@ -11,7 +11,12 @@ var winston = require('winston');
   var transport = new winston.transports.DailyRotateFile(config.logrotate);
 
   transport.on('rotate', function(oldFilename, newFilename) {
-    // do something fun
+      const json2html = require('node-json2html');
+      var html = json2html.transform(JSON.parse(fs.readFileSync(oldFilename)),{"<>":"li","html":"<small>${index}</small><pre>${value}</pre>"});
+      if(fs.existsSync(oldFilename+'.prev.html')){
+         fs.unlink(oldFilename+'.prev.html');
+      }
+      fs.writeFileSync(newFilename+'.prev.html', html);
       console.log('Log rotation: ', [oldFilename, newFilename]);
   });
 
