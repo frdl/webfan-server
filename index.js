b9085aa3c52eab74f19143b41477674dea404aa4
Till Wehowski <github@webfan.de>
2021-07-15 22:00:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,90 @@
 
-
 'use strict';
+
+
+var deepMerge = require('@betafcc/deep-merge');
+const arrayDeepMerge = deepMerge.addCase(
+    [Array.isArray, Array.isArray],
+    (a, b) => a.concat(b)
+);
 
 var prop = Object.defineProperty;
 
+function swapObjectFlip(obj) {
+  return Object.keys(obj).reduce((ret, key) => {
+    ret[obj[key]] = key;
+    return ret;
+  }, {});
+}
 
 function Server(){
   
 }
 
-
-Server.prototype.callBackApp =  (app, cb) =>{
-	 var that=this;
+const _methdoAlias ={
+	'stop': 'close',
+	'start': 'listen',	
+};
+Server.prototype.callBackApp =  (app, cb, args) =>{
+	 var that=this, alias2 = swapObjectFlip(_methdoAlias);
 	 if('string'=== typeof cb){
-					 return that[cb](app);
-				 }else if('function'=== typeof cb){
-					  return cb(app);
-				 }else{
-				     return app; 
-	}
+		  if('function'!==typeof app.app[cb]){
+			  if('string'===typeof _methdoAlias[cb]){
+				  cb = _methdoAlias[cb];
+			  }else if('string'===typeof alias2[cb]){
+				  cb = alias2[cb];
+			  }
+		  }
+		  if('function'!==typeof app.app[cb]){
+			  throw new Error(cb + ' is not a function of '+app.name);
+		  }
+		  return app.app[cb].apply(app, args);
+	  }else if('function'=== typeof cb){
+		 return cb.apply(app,args);
+	  }else{
+	    return app; 
+	 }
 };
 
 
-Server.prototype.getApp = (ix, cb) =>{
+Server.prototype.getApp = (ix, cb, args) =>{
 	 var that=this;
 		      if(!isNaN(ix)){
 				 var app = that.apps[ix];
-			         return  that.callBackApp(app, cb);
+			         return  that.callBackApp(app, cb, args);
    
 		      }else{
-			  return that.queryApps(ix, cb);    
+			  return that.queryApps(ix, cb, args);    
 		      }
 };
 
-Server.prototype.queryApps = (arg, cb) => {
+Server.prototype.queryApps = (arg, cb, args) => {
 	var that=this;
 		      if(!isNaN(arg)){
-				 return that.getApp(arg, cb); 
+				 return that.getApp(arg, cb, args); 
 		      }else if('string'===typeof arg){
 			     for(var i=0;i<that.apps.length;i++){
 				 var app = that.apps[i];
 				 if(arg === app.name){
-					 that.callBackApp(app, cb);
+					 that.callBackApp(app, cb, args);
 				 }else if('undefined'!==typeof app.type){
 				   var type = app.type.split(/[\#\s\/\.]/);
 				   var query = arg.split(/\#/);	 
 				   if((type[0] === query[0] || '*' === query[0]) 
 					  && (type[1] === query[1] || '*' === query[1])
 					  && ('undefined'===typeof query[2] || type[2] === query[2] || '*' === query[2] || '' === query[2]) ){
-				     	that.callBackApp(app, cb);
+				     	that.callBackApp(app, cb, args);
 				   }
 				 }
 			     }
 		      }else if('object' === typeof arg && 'undefined'!==typeof arg.key){
 			    if('undefined'===typeof arg.value){
-				  that.queryApps(arg.key + '#*', cb);   
+				  that.queryApps(arg.key + '#*', cb, args);   
 			    }else{
 				  for(var i=0;i<that.apps.length;i++){
 					 var app = that.apps[i];
 					  if('undefined'!==typeof app[arg.key] && app[arg.key] === arg.value){
-						   that.getApp(i, cb); 
+						   that.getApp(i, cb, args); 
 					  }
 				   }
 			     }
@@ -68,25 +93,48 @@ Server.prototype.queryApps = (arg, cb) => {
 
 
 Server.prototype.close = arg => {		   
-	return this.queryApps(arg, 'close');   	
+	return this.queryApps(arg, 'close', []);   	
 };
 
-Server.prototype.constructor=function(){
+Server.prototype.constructor=function(options){
   
-
+  var _conf = arrayDeepMerge(require('./get-configuration'), options.config || {});
   var props = {},that=this;
   var _apps = [];	
-  
-		
+	
+  prop(that, 'addApp', {
+  	get : ()=>{
+		return (app, type, name) => {
+			that.apps = {
+			    name:name,
+				type : type,
+				app:app
+			};		  
+		};
+	  }
+  });	  
+	
+  prop(that, 'config', {
+  	get : ()=>{
+		  return _conf;
+	  }
+  });		
   prop(that, 'apps', {
   	get : ()=>{
 		  return _apps;
+	  },
+	  set : (app)=>{
+		  _apps.push({
+			    name:app.name || 'App' + _apps.length.toString(),
+				type : app.type || 'UNDEFINED#UNDEFINED#UNDEFINED' + _apps.length.toString(),
+				app:app.app || function(mount, req,res,next){}
+			});
 	  }
   });	  
   prop(that, 'close', {
   	get : ()=>{
 		  return arg => {
-		      that.queryApps(arg, 'close');   
+		      that.queryApps(arg, 'close', ['*.*.*']);   
 		  };
 	  }
   });    
@@ -181,7 +229,9 @@ Server.prototype.create = () =>{
 
   prop(Server, 'create', {
   	get : ()=>{
-		  return (new Server).create;
+		return options => {
+		  return (new Server(options)).create(options.config || {});
+		};
 	  }
   });	
 module.exports = Server;
