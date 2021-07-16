00cf80c115c0285fcbc00e8d2f707581699bce31
Till Wehowski <github@webfan.de>
2021-07-16 15:02:52 +0200

    Update webfan-server.config.dist.js

--- webfan-server.config.dist.js
+++ webfan-server.config.dist.js
@@ -1,22 +1,42 @@
 
 
-
+var fs = require('fs');
+var deepMerge = require('@betafcc/deep-merge');
 var ip = require('ip');
+
+const arrayDeepMerge = deepMerge.addCase(
+    [Array.isArray, Array.isArray],
+    (a, b) => a.concat(b)
+);
+
+
+async function logMessage(type, args){
+	var method2 = ('function'===typeof console[type]) ? console[type] : console.log;
+	method2(...args);
+	
+      var logger=require('./logging');
+       var method = ('function'===typeof logger[type]) ? logger[type] : logger.log;
+	method(...args);	
+	
+}
+
 var myIp = ip.address();
+
+//var target = '212.72.182.211';
+var target = 'https://frdl.ws/frdlwebuserworkspace/default.domain';
 
 var Port = process.env.port;
 
-var target = '212.72.182.211';
-//var target = 'https://frdl.ws/frdlwebuserworkspace';
+//var target = '212.72.182.211';
+var target = 'https://frdl.ws/frdlwebuserworkspace';
 
-var config = {
+var config = { 
+ //balancers : ['localhost:6000', 'localhost:6001', 'localhost:6002'],
+ balancers : [],
  vhosts : {
-	//balancers : ['localhost:6000', 'localhost:6001', 'localhost:6002'],
-	balancers : [],
 	dir : __dirname + '/www/vhosts/',
 	proxyfile : 'proxy.json',
-	proxymodule : 'proxyhandler.js',
-	domainmetrics : '.metrics.js',
+	proxymodule : 'proxyhandler',
 	docroot : 'httpdocs',
 	default : {
 		  port : Port,
@@ -42,7 +62,33 @@ var config = {
 	   xfwd: false,
            prependPath: false  // ,
        //    keepAlive: false
-  }
+  },
+  log: {
+        debug: function() {
+	 logMessage('debug', ...arguments);
+	},
+        notice: function() {
+	  logMessage('notice', ...arguments);
+	},
+        log: function() {
+	   logMessage('log', ...arguments);
+	},
+        warn: function() {
+	   logMessage('warn', ...arguments);
+	},
+        info: function() {
+	  logMessage('info', ...arguments);
+	},
+        error: function(err) {
+            if (err.stack) {
+                console.error(err.stack);
+		    logMessage('error', [err.stack]);
+            } else {
+                console.error(err);
+		    logMessage('error', [err]);
+            }
+        }
+    }
  },
  logrotate:{
     frequency : '1h',	 
@@ -54,5 +100,6 @@ var config = {
     maxFiles: '7d'
   }
 };
+
 
 module.exports = config;
